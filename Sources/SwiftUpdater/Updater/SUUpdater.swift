c1925e23cc4cfebef735808ab67f4bc6aea81c5e
Jochen Bernard <jochenbernard@gmail.com>
2025-10-04 19:16:01 +0200

    Improve SUUpdater DocC

--- Sources/SwiftUpdater/Updater/SUUpdater.swift
+++ Sources/SwiftUpdater/Updater/SUUpdater.swift
@@ -2,16 +2,21 @@ import Foundation
 
 /// An object that manages updates.
 public final class SUUpdater: Sendable {
+    /// The bundle to update.
     public let bundle: Bundle
+
+    /// The downloader to download updates.
     public let downloader: SUUpdateDownloader
+
+    /// The extractor to extract updates.
     public let extractor: SUUpdateExtractor?
 
     /// Creates an updater for the specified bundle using the specified downloader and extractor.
     ///
     /// - Parameters:
-    ///   - bundle: The bundle. The default is `main`.
-    ///   - downloader: The downloader. The default is ``SUUpdateDownloader/standard(urlSession:)``.
-    ///   - extractor: The extractor. The default is `nil`.
+    ///   - bundle: The bundle to update. The default is `main`.
+    ///   - downloader: The downloader to download updates. The default is ``SUUpdateDownloader/standard(urlSession:)``.
+    ///   - extractor: The extractor to extract updates. The default is `nil`.
     public init(
         bundle: Bundle = .main,
         downloader: SUUpdateDownloader = .standard(),
